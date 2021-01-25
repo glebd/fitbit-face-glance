b336b85fc41239ba667dd733561c71390136430f
Gleb Dolgich <glebd70@gmail.com>
2021-01-25 18:35:44 +0000

    WIP Add minute hand and clock functionality

--- glance/app/index.js
+++ glance/app/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+import clock from "clock";
+import document from "document";
+
+let myHours = $("hours");
+let myMins = $("minutes");
+let mySecs = $("seconds");
+
+function $(s) {
+  return document.getElementById(s);
+}
+
+function onTick(now) {
+  now = (now && now.date) || new Date();
+
+  let hours = now.getHours() % 12;
+  let mins = now.getMinutes();
+  let secs = now.getSeconds();
+
+  myHours.groupTransform.rotate.angle = (hours + mins/60)*30;
+  myMins.groupTransform.rotate.angle = mins*6;
+  mySecs.groupTransform.rotate.angle = secs*6;
+}
+
+clock.granularity = "seconds";
+clock.ontick = onTick;
+onTick();
